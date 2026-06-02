275cdeb0ad8952b2defe98c1bd974e9f99b1efd0
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-06-02 10:57:54 +0000

    chore: modified release config

--- release.config.ts
+++ release.config.ts
@@ -3,7 +3,10 @@ import type { GlobalConfig } from "semantic-release";
 const config: GlobalConfig = {
   tagFormat: "v${version}",
   repositoryUrl: "https://github.com/TakahashiNguyen/Efficia",
-  branches: ["main", { name: "dev-*", prerelease: "dev" }],
+  branches: ["main", {
+    name: "dev-*", prerelease: "${name.replace('dev-', '')}",
+    channel: "${name.replace('dev-', '')}",
+  }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
